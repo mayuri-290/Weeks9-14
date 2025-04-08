7cd181227f28c05468fcf4940527c57f9d996e4b
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-04-07 22:25:33 -0400

    fixed errors of player not interact with chests.

--- Assets/SystemAssignment/chests.cs
+++ Assets/SystemAssignment/chests.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class chests : MonoBehaviour
 {
     public int score = 0;
-    public float touchRange = 0.3f;
+    public float touchRange = 0.5f;
+    public Sprite openChest;
+    public Transform playerPos;
+
+
+    public List<GameObject> chestList;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //chestList = new List<GameObject>(); Problems occurs when adding this initializing list. Because it will reset the condition of chests.
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < chestList.Count; i++)
+        {
+            GameObject chest = chestList[i];
+
+
+
+            float distance = Vector2.Distance(playerPos.transform.position, chest.transform.position);
+
+            if (distance < touchRange)
+            {
+                SpriteRenderer collectedChest = chest.GetComponent<SpriteRenderer>();
+
+                if (collectedChest.sprite != openChest)
+                {
+                    collectedChest.sprite = openChest;
+                    score += 1;
+                    Debug.Log("Collected a chest!");
+                }
+            }
+        }
     }
 }
